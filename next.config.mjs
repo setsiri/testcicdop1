0efca691642a6dc2015f75365e1090651c57c04a
setsiri matewin <setsiri_matewin@cmu.ac.th>
2024-11-20 13:26:54 +0700

    push

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-    output: "export",
+    
     basePath: "/testcicdop1",
     images: {
         unoptimized: true,
